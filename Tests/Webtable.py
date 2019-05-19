9f40f8376595b3d38afdc756ef9863f7be1869d0
SocialVegan <nagendra256@gmail.com>
2019-05-19 17:35:32 +0530

    added batch file

--- Tests/Webtable.py
+++ Tests/Webtable.py
@@ -6,9 +6,35 @@ driver.maximize_window()
 driver.implicitly_wait(30)
 
 row_length=driver.find_elements_by_xpath("//*[@id='emp']/tbody/tr")
-print(len(row_length))
+print('row length is',len(row_length))
 column_length=driver.find_elements_by_xpath("//*[@id='emp']/thead/tr/th")
-print(len(column_length))
-first_part="//*[@id='emp']/thead/tr/th["
-second_part="]"
+print('column legnth is',len(column_length))
+header_data=driver.find_elements_by_xpath("//*[@id='emp']/thead/tr/th")
+print('header count is',len(header_data))
+for data in header_data:
+    result=data
+    print(result.text)
+'''second row data'''
 
+table_data=driver.find_elements_by_xpath("//*[@id='emp']")
+print('-----Entire Table Data------')
+for data in table_data:
+    result=data
+    print(result.text)
+
+
+first_column=driver.find_elements_by_xpath("//tr/td[1]")
+for data in first_column:
+    result=data
+    print('First Column data',result.text)
+third_column=driver.find_elements_by_xpath("//tr/td[3]")
+for data in third_column:
+    result=data
+    print('Third Column data',result.text)
+'''to check if the employee in the second row is getting correct salary mentioned in the table'''
+first_part="//*[@id='emp']/tbody/tr[1]"
+second_part="/td["
+third_part="]"
+for i in column_length:
+    res=i
+    print(res.text)
--- Tests/Window Handling.py
+++ Tests/Window Handling.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 import time
 driver=webdriver.Chrome()
-driver.get("http://phptravels.com/demo")
+driver.get("http://citibank.co.in")
 driver.maximize_window()
 driver.implicitly_wait(20)
 driver.delete_all_cookies()
-'''current_window_id=driver.current_window_handle
+current_window_id=driver.current_window_handle
 print(current_window_id)
 time.sleep(10)
-driver.find_element_by_xpath("//span[text()='Login']").click()
+driver.find_element_by_xpath("//a[@title='Login']").click()
 window_ids=driver.window_handles
 print(type(window_ids))
 print(window_ids)
@@ -16,5 +16,5 @@ for handle in window_ids:
     print(handle)
     if handle !=current_window_id:
         driver.switch_to.window(handle)
-        driver.find_element_by_name("username").send_keys('Dummyuser')
-driver.quit()'''
+        driver.find_element_by_xpath("//input[@id='User_Id']").send_keys('Dummyuser')
+driver.quit()
